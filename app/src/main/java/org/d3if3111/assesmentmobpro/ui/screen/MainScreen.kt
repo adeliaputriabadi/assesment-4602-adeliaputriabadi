cd214d3a655bfcfb0fdcc530c29032d9fceeb1b0
Adelia Putri Abadi <adeliaptrbd2002@gmail.com>
2024-04-02 18:28:01 +0700

    Membuat perhitungan diskon dan memastikan input sesuai harapan

--- app/src/main/java/org/d3if3111/assesmentmobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3111/assesmentmobpro/ui/screen/MainScreen.kt
@@ -1,17 +1,10 @@
 package org.d3if3111.assesmentmobpro.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.unit.dp
-import org.d3if3111.assesmentmobpro.R
-
-
 
 import android.content.res.Configuration
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,21 +15,26 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,24 +44,10 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.d3if3111.assesmentmobpro.R
 import org.d3if3111.assesmentmobpro.ui.theme.AssesmentMobproTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AssesmentMobproTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
-            }
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -88,13 +72,21 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier) {
     var hargaAwal by remember { mutableStateOf("") }
+    var hargaAwalError by remember { mutableStateOf(false) }
+
     var persentaseDiskon by remember { mutableStateOf("") }
+    var persentaseDiskonError by remember { mutableStateOf(false) }
+
 
     val radioOptions = listOf(
         stringResource(id = R.string.harga_diskon),
         stringResource(id = R.string.total_bayar),
 
         )
+
+    var harga by rememberSaveable { mutableFloatStateOf(0f) }
+
+
     var kategori by remember { mutableStateOf(radioOptions[0]) }
     Column(
         modifier = modifier
@@ -113,7 +105,9 @@ fun ScreenContent(modifier: Modifier) {
             value = hargaAwal,
             onValueChange = { hargaAwal = it },
             label = { Text(text = stringResource(R.string.harga_awal)) },
-            leadingIcon = { Text(text = "Rp")},
+            isError = hargaAwalError,
+            leadingIcon = { IconPicker(hargaAwalError,"Rp") },
+            supportingText = { ErrorHint(hargaAwalError)},
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -125,7 +119,9 @@ fun ScreenContent(modifier: Modifier) {
             value = persentaseDiskon,
             onValueChange = { persentaseDiskon = it },
             label = { Text(text = stringResource(R.string.persentase_diskon)) },
-            trailingIcon = { Text(text = "%") },
+            isError = persentaseDiskonError,
+            trailingIcon = { IconPicker(persentaseDiskonError, "%") },
+            supportingText = { ErrorHint(persentaseDiskonError)},
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -158,7 +154,16 @@ fun ScreenContent(modifier: Modifier) {
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Button(
-                onClick = {},
+                onClick = {
+                          hargaAwalError = (hargaAwal == "" || hargaAwal == "0")
+                          persentaseDiskonError = (persentaseDiskon == "" || persentaseDiskon == "0")
+                          if (hargaAwalError || persentaseDiskonError) return@Button
+
+                            harga = hitungRumus(hargaAwal.toFloat(), persentaseDiskon.toFloat(), kategori == radioOptions[0])
+
+
+
+                },
                 modifier = Modifier.padding(top = 8.dp),
                 contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF58A399))
@@ -169,6 +174,8 @@ fun ScreenContent(modifier: Modifier) {
                 onClick = {
                     hargaAwal = ""
                     persentaseDiskon = ""
+                    harga = 0f
+                    //rumusTotal = 0f
                 },
                 modifier = Modifier.padding(top = 8.dp),
                 contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp) ,
@@ -177,8 +184,52 @@ fun ScreenContent(modifier: Modifier) {
                 Text(text = stringResource(id = R.string.reset))
             }
         }
+        if (harga != 0f ){
+            Divider(
+                modifier =  Modifier.padding(vertical = 16.dp),
+                thickness = 1.dp
+            )
+
+
+           Text(text = stringResource(id = R.string.rumus_total, harga))
+        }
+
+    }
+
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit : String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+
+    }
+    else {
+        Text(text = unit)
     }
 }
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(id = R.string.input_invalid))
+    }
+}
+
+
+
+private fun hitungRumus(hargaAwal: Float, persentaseDiskon: Float, isDiskon:Boolean): Float {
+    return if(isDiskon) {
+        hargaAwal * (persentaseDiskon / 100)
+    }
+    else {
+        hargaAwal - (hargaAwal * (persentaseDiskon /100))
+    }
+}
+
+
+
+
 
 
     @Composable
@@ -195,6 +246,9 @@ fun ScreenContent(modifier: Modifier) {
             )
         }
     }
+
+
+
 
 
 
