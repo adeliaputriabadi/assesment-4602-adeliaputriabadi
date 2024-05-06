4d8391f34b48a30c527432cf0c66e18d77d4b2b3
Adelia Putri Abadi <adeliaptrbd2002@gmail.com>
2024-05-06 22:00:17 +0700

    menyiapkan data di ViewModel

---
+++ app/src/main/java/org/d3if3111/assesmentmobpro/model/Uang.kt
@@ -0,0 +1,9 @@
+package org.d3if3111.assesmentmobpro.model
+
+data class Uang(
+    val id: Long,
+    val keterangan: String,
+    val nominal: String,
+    val kategori: String,
+    //val tanggal: String
+)
---
+++ app/src/main/java/org/d3if3111/assesmentmobpro/ui/screen/MainViewModel.kt
@@ -0,0 +1,61 @@
+package org.d3if3111.assesmentmobpro.ui.screen
+
+import androidx.lifecycle.ViewModel
+import org.d3if3111.assesmentmobpro.model.Uang
+
+class MainViewModel : ViewModel() {
+
+    val data = getDataDummy()
+    private fun getDataDummy(): List<Uang> {
+        val keterangan = arrayOf(
+            "Beli Kuota",
+            "Penghasilan dari tiktok",
+            "Beli makan",
+            "Pendapatan penjualan shopee",
+            "Beli baju",
+            "Beli Kuota",
+            "Penghasilan dari tiktok",
+            "Beli makan",
+            "Pendapatan penjualan shopee",
+            "Beli baju"
+
+        )
+        val nominal = arrayOf(
+            "30000",
+            "1000000",
+            "20000",
+            "500000",
+            "300000",
+            "30000",
+            "1000000",
+            "20000",
+            "500000",
+            "300000",
+
+        )
+        val kategori = arrayOf(
+            "Pengeluaran",
+            "Penghasilan",
+            "Pengeluaran",
+            "Penghasilan",
+            "Pengeluaran",
+            "Pengeluaran",
+            "Penghasilan",
+            "Pengeluaran",
+            "Penghasilan",
+            "Pengeluaran"
+        )
+        val data = mutableListOf<Uang>()
+        for (i in keterangan.indices) {
+            data.add(
+                Uang(
+                    (i + 1).toLong(),
+                    keterangan[i],
+                    nominal[i],
+                    kategori[i]
+                )
+            )
+        }
+        return data
+    }
+}
