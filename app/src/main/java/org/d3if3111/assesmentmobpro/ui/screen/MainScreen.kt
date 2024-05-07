e82df828ca5873e501c086296d85357f1d12f1aa
Adelia Putri Abadi <adeliaptrbd2002@gmail.com>
2024-05-07 16:49:36 +0700

    Membuat tampilan staggered grid

--- app/src/main/java/org/d3if3111/assesmentmobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3111/assesmentmobpro/ui/screen/MainScreen.kt
@@ -2,6 +2,7 @@ package org.d3if3111.assesmentmobpro.ui.screen
 
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,8 +12,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -31,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -96,12 +103,12 @@ fun MainScreen(navController: NavHostController) {
             }
         }
     ) { padding ->
-        ScreenContent(Modifier.padding(padding), navController)
+        ScreenContent(showList, Modifier.padding(padding), navController)
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier, navController: NavHostController) {
+fun ScreenContent(showList: Boolean, modifier: Modifier, navController: NavHostController) {
    val context = LocalContext.current
     val db = UangDb.getInstance(context)
     val factory = ViewModelFactory(db.dao)
@@ -122,6 +129,7 @@ fun ScreenContent(modifier: Modifier, navController: NavHostController) {
         }
     }
     else {
+        if (showList) {
         LazyColumn (
             modifier = modifier.fillMaxSize(),
             contentPadding = PaddingValues(bottom = 84.dp)
@@ -132,6 +140,24 @@ fun ScreenContent(modifier: Modifier, navController: NavHostController) {
                 }
                 Divider()
             }
+        }
+    } else {
+        LazyVerticalStaggeredGrid(
+            modifier = modifier.fillMaxSize(),
+            columns = StaggeredGridCells.Fixed(2),
+            verticalItemSpacing = 8.dp,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 84.dp)
+        ) {
+            items(data) {
+                GridItem(uang = it) {
+                    navController.navigate(Screen.FormUbah.withId(it.id))
+
+                }
+            }
+        }
+
+
         }
     }
 }
@@ -162,6 +188,41 @@ fun ListItem(uang: Uang, onClick: () -> Unit) {
         Text(text = uang.kategori
         )
         Text(text = uang.tanggal)
+    }
+}
+
+@Composable
+fun GridItem(uang: Uang, onClick: () -> Unit) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ){
+            Text(
+                text = uang.keterangan,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+                )
+            Text(
+                text =uang.nominal,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+                )
+            Text(text = uang.kategori
+                )
+            Text(text = uang.tanggal)
+
+        }
+
     }
 }
 
