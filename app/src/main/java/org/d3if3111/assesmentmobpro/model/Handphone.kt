3ef7ea409671846f0fdeb068ec9828051343e8fb
Adelia Putri Abadi <adeliaptrbd2002@gmail.com>
2024-06-10 21:23:42 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/org/d3if3111/assesmentmobpro/model/Handphone.kt
@@ -0,0 +1,7 @@
+package org.d3if3111.assesmentmobpro.model
+
+data class Handphone(
+    val name: String,
+    val type: String,
+    val imageId: String
+)
--- app/src/main/java/org/d3if3111/assesmentmobpro/network/HandphoneApiService.kt
+++ app/src/main/java/org/d3if3111/assesmentmobpro/network/HandphoneApiService.kt
@@ -1,17 +1,26 @@
 package org.d3if3111.assesmentmobpro.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if3111.assesmentmobpro.model.Handphone
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/adeliaputriabadi/assesment-4602-adeliaputriabadi/static-api/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 interface HandphoneApiService {
     @GET("handphone.json")
-    suspend fun getHandphone(): String
+    suspend fun getHandphone(): List<Handphone>
 }
 
 object HandphoneApi {
